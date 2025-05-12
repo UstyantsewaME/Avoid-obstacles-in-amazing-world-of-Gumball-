15aa85a1a254fcfaa494e933fea17563eb300531
zero_ <zero_@BSHKSPC>
2025-05-12 13:45:23 +0500

    Добавьте файлы проекта.

---
+++ Avoid obstacles in amazing world of Gumball!/MainMenu.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Avoid_obstacles_in_amazing_world_of_Gumball_
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.settingsBTN = new System.Windows.Forms.Button();
+            this.playBTN = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.Github_12_05_2025;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(3, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 59);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // settingsBTN
+            // 
+            this.settingsBTN.AutoSize = true;
+            this.settingsBTN.BackColor = System.Drawing.Color.Transparent;
+            this.settingsBTN.BackgroundImage = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.Settings;
+            this.settingsBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.settingsBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.settingsBTN.FlatAppearance.BorderSize = 0;
+            this.settingsBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.settingsBTN.Location = new System.Drawing.Point(3, 240);
+            this.settingsBTN.Name = "settingsBTN";
+            this.settingsBTN.Size = new System.Drawing.Size(313, 70);
+            this.settingsBTN.TabIndex = 2;
+            this.settingsBTN.UseVisualStyleBackColor = false;
+            // 
+            // playBTN
+            // 
+            this.playBTN.BackColor = System.Drawing.Color.Transparent;
+            this.playBTN.BackgroundImage = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.Play;
+            this.playBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.playBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playBTN.FlatAppearance.BorderSize = 0;
+            this.playBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.playBTN.Location = new System.Drawing.Point(3, 179);
+            this.playBTN.Name = "playBTN";
+            this.playBTN.Size = new System.Drawing.Size(175, 57);
+            this.playBTN.TabIndex = 1;
+            this.playBTN.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(96, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitBTN
+            // 
+            this.exitBTN.BackColor = System.Drawing.Color.Transparent;
+            this.exitBTN.BackgroundImage = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.Exit;
+            this.exitBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.exitBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitBTN.FlatAppearance.BorderSize = 0;
+            this.exitBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exitBTN.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.exitBTN.Location = new System.Drawing.Point(3, 381);
+            this.exitBTN.Name = "exitBTN";
+            this.exitBTN.Size = new System.Drawing.Size(170, 57);
+            this.exitBTN.TabIndex = 3;
+            this.exitBTN.UseVisualStyleBackColor = false;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = global::Avoid_obstacles_in_amazing_world_of_Gumball_.Properties.Resources.wallpaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(398, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exitBTN);
+            this.Controls.Add(this.settingsBTN);
+            this.Controls.Add(this.playBTN);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Avoid, Gumball!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button playBTN;
+        private System.Windows.Forms.Button settingsBTN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exitBTN;
+    }
+}
+
---
+++ Avoid obstacles in amazing world of Gumball!/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avoid_obstacles_in_amazing_world_of_Gumball_
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
